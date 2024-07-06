370617dd0034a43209916d4a7944defb3fd84356
Shun Yamanouchi <yamanouchi@bs.s.u-tokyo.ac.jp>
2024-07-06 20:44:57 +0900

    compile the fish dataset

--- config/myconfig.py
+++ config/myconfig.py
@@ -3,3 +3,5 @@ from pathlib import Path
 ROOT_DIR = Path('/home/jovyan')
 DATA_DIR = ROOT_DIR / 'data'
 DATASET_DIR = ROOT_DIR / 'datasets'
+PUBDATA_DIR = DATA_DIR / 'pubdata'
+METADATA_DIR = ROOT_DIR / 'metadata'
