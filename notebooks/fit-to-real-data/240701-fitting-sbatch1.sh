57cf7b3ec5a6abf12d6a648a07333998076def93
Shun Yamanouchi <yamanouchi@bs.s.u-tokyo.ac.jp>
2024-07-06 13:55:39 +0900

    fitting for bact

---
+++ notebooks/fit-to-real-data/240701-fitting-sbatch1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --array 1-1000%80
+#SBATCH --output data/batch/02-bacteria/log/%x.stdxxx-%3a
+set -eu -o pipefail
+
+cd $SLURM_SUBMIT_DIR
+eval `awk -v ARRAYID=$(( $SLURM_ARRAY_TASK_ID+1 )) -F "\t" 'NR==ARRAYID {print $NF}' notebooks/fit-to-real-data/240701-batch-job-array-bacteria.txt`
