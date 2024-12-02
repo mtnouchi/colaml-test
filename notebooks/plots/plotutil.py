f911275674b5b70429edd46e983f564bfeae5125
Shun Yamanouchi <yamanouchi@bs.s.u-tokyo.ac.jp>
2024-12-02 19:59:42 +0900

    add final images

---
+++ notebooks/plots/plotutil.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mm = 1 / 25.4 # inch -> mm
+
+OkabeItoCUDcy = plt.cycler(
+    color=['#0072B2','#E69F00','#009E73','#D55E00','#CC79A7','#F0E442','#56B4E9']
+)
